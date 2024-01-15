ef75eca7ef7712a82b0342864181e9941309f768
pwbyl2 <156655908+pwbyl2@users.noreply.github.com>
2024-01-15 11:00:56 +0100

    Create ping.ps1

---
+++ ping.ps1
@@ -0,0 +1,1 @@
+ping.exe -t ping -t 127.0.0.1|cmd /q /v /c "(pause&pause)>nul & for /l %%a in () do (set /p "data=" && echo(!date! !time! !data!)&ping -n 2 127.0.0.1>nul" >>C:\testy\test.txt|Foreach{"{0} - {1}" -f (Get-Date),$_} | Tee C:\testy\test.txt
